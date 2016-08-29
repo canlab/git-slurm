1941625c354610ccd8350f792e5fa5d23d7af893
Daniel Weflen <danweflen@gmail.com>
2016-08-29 10:10:44 -0600

    Added a slurm submission script.

---
+++ slurm.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Wall Clock time, maximum of 300 minutes
+#SBATCH -t 300
+
+#Put stdout and stderr in a single file
+#SBATCH -o ~/slurm-output.txt
+
+#Sets my email
+#SBATCH --mail-user=Choongwan.Woo@colorado.edu
+
+#Names the job
+#SBATCH -
+
+
+
